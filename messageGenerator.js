950d53f61baf0486bbf6c17fd0156b664a9564ec
mchallinor85 <117482265+mchallinor85@users.noreply.github.com>
2023-10-25 05:17:56 +0100

    first commit basic random logs

---
+++ messageGenerator.js
@@ -0,0 +1,5 @@
+
+const rand = Math.floor(Math.random() * 5);
+const msg = ["This could be your lucky day!", "Take a chance!", "Do it!", "Not today!", "Leave it for another day!"];
+
+console.log(msg[rand]);
