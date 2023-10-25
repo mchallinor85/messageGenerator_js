2e80b8b986b13952d96904b5da2d71746dc6c0cd
mchallinor85 <117482265+mchallinor85@users.noreply.github.com>
2023-10-25 05:23:09 +0100

    Updated messageGenerator.js "Looks less likely"

--- messageGenerator.js
+++ messageGenerator.js
@@ -1,5 +1,5 @@
 
-const rand = Math.floor(Math.random() * 5);
-const msg = ["This could be your lucky day!", "Take a chance!", "Do it!", "Not today!", "Leave it for another day!"];
+const rand = Math.floor(Math.random() * 6);
+const msg = ["This could be your lucky day!", "Take a chance!", "Do it!", "Not today!", "Leave it for another day!", "Looks less likely"];
 
 console.log(msg[rand]);
